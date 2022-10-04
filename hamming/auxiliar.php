1b6f19cd1afe79145f2751a9863631ec986874f6
JaviRomero00 <javier.romero.perez@iesdonana.org>
2022-10-04 21:20:54 +0200

    hamming

---
+++ hamming/auxiliar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Javier Romero Pérez
+ * @copyright Copyright (c) 2022 Javier Romero Pérez
+ * @license https://www.gnu.org/license/gpl.txt
+ */
+
+function hamming($ham1, $ham2)
+{
+    if (strlen($ham1) == strlen($ham2)) {
+        for ($i = 0, $i <= strlen($ham1), $i++) {
+            ;
+        }
+    }
+}
---
+++ hamming/hamming.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hamming</title>
+</head>
+<body>
+    <?php
+    $adn1 = trim($_GET['adn1']);
+    $adn2 = trim($_GET['adn2']);
+    $ham = trim($_GET['ham']);
+    ?>
+</body>
+</html>
